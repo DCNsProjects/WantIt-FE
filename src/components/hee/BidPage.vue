20a612817d82e800b56a5cd8e3f73eda8df55c2c
HEEEUN9812 <154495684+HEEEUN9812@users.noreply.github.com>
2024-04-14 16:46:14 +0900

    Add files via upload

---
+++ src/components/hee/BidPage.vue
@@ -0,0 +1,352 @@
+<template>
+  <div class="black-bg" v-if="호가정책 == true" style="">
+    <div class="white-bg">
+      <div>
+        <h4>경매 호가표</h4>
+        <button
+          type="button"
+          class="btn-close top"
+          aria-label="Close"
+          @click="호가정책 = false"
+        ></button>
+      </div>
+      <table class="table">
+        <thead class="table-light">
+          <tr>
+            <th scope="col">현재가 이상 (원)</th>
+            <th scope="col">현재가 미만 (원)</th>
+            <th scope="col">호가 단위 (원)</th>
+          </tr>
+        </thead>
+        <tbody class="table-group-divider">
+          <tr>
+            <td>~</td>
+            <td>50만</td>
+            <td>1만</td>
+          </tr>
+          <tr>
+            <td>50만</td>
+            <td>100만</td>
+            <td>5만</td>
+          </tr>
+          <tr>
+            <td>100만</td>
+            <td>300만</td>
+            <td>10만</td>
+          </tr>
+          <tr>
+            <td>300만</td>
+            <td>500만</td>
+            <td>20만</td>
+          </tr>
+          <tr>
+            <td>500만</td>
+            <td>1,000만</td>
+            <td>30만</td>
+          </tr>
+          <tr>
+            <td>1,000만</td>
+            <td>3,000만</td>
+            <td>50만</td>
+          </tr>
+          <tr>
+            <td>3,000만</td>
+            <td>5,000만</td>
+            <td>100만</td>
+          </tr>
+          <tr>
+            <td>5,000만</td>
+            <td>1억</td>
+            <td>200만</td>
+          </tr>
+          <tr>
+            <td>1억</td>
+            <td>2억</td>
+            <td>300만</td>
+          </tr>
+          <tr>
+            <td>2억</td>
+            <td>~</td>
+            <td>500만</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+
+  <div class="black-bg" v-if="낙찰수수료 == true">
+    <div class="white-bg">
+      <div>
+        <h4>낙찰 수수료</h4>
+        <button
+          type="button"
+          class="btn-close top"
+          aria-label="Close"
+          @click="낙찰수수료 = false"
+        ></button>
+      </div>
+      <table class="table">
+        <thead class="table-light">
+          <tr>
+            <th scope="col">국내 경매</th>
+          </tr>
+        </thead>
+        <tbody class="table-group-divider">
+          <tr>
+            <td>일괄 20%</td>
+          </tr>
+        </tbody>
+      </table>
+      <div>- 판매자에게 판매금액에 대한 수수료 부과</div>
+    </div>
+  </div>
+
+  <div class="black-bg" v-if="입찰하기 == true">
+    <div class="white-bg">
+      <button
+        type="button"
+        class="btn-close top"
+        aria-label="Close"
+        @click="입찰하기 = false"
+      ></button>
+      <div class="pwWrap">
+        <div class="pwSection">
+          <span class="dot"></span>
+          <span class="dot"></span>
+          <span class="dot"></span>
+          <span class="dot"></span>
+          <p class="message">&nbsp;</p>
+        </div>
+        <div class="numberSection">
+          <button class="number">1</button>
+          <button class="number">2</button>
+          <button class="number">3</button>
+          <button class="number">4</button>
+          <button class="number">5</button>
+          <button class="number">6</button>
+          <button class="number">7</button>
+          <button class="number">8</button>
+          <button class="number">9</button>
+          <button class="number">0</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="split-screen">
+    <div class="left">
+      <div class="product-img">
+        <img alt="Vue logo" src="./../assets/logo.png" />
+      </div>
+    </div>
+    <div class="v-line"></div>
+    <div class="right">
+      <div class="price-wrapper">
+        <div class="price-wrapper2">
+          <div class="max-price">현재 최고가</div>
+          <div class="price">
+            {{ 실시간호가 }}
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-body">
+          <h4 class="product name">{{ 상품명 }}</h4>
+          <div>{{ 상품설명 }}</div>
+        </div>
+      </div>
+      <div class="date">경매 종료일: {{ 경매종료일 }}</div>
+      <hr />
+      <div class="d-grid gap-4 d-md-flex justify-content-md-end bid-btn">
+        <button class="btn btn-secondary w-75" type="button" @click="입찰하기 = true">
+          입찰하기
+        </button>
+        <button class="btn btn-secondary w-25" type="button">♥</button>
+      </div>
+
+      <div
+        class="d-grid gap-2 d-md-flex justify-content-md-end"
+        style="margin-right: 50px"
+      >
+        <button
+          class="btn btn-outline-secondary me-md-2"
+          type="button"
+          @click="호가정책 = true"
+        >
+          호가 정책
+        </button>
+        <button
+          class="btn btn-outline-secondary"
+          type="button"
+          @click="낙찰수수료 = true"
+        >
+          수수료 정책
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      호가정책: false,
+      낙찰수수료: false,
+      입찰하기: false,
+      실시간호가: "300,000원",
+      경매종료일: "2024/04/30",
+      상품명: "골동품 춘식이",
+      상품설명:
+        "물건을 경매하여 쉽게 거래할 수 있는 편리한 플랫폼 Wantit! 안전한 결제 시스템과 실시간 경매 진행 상황 확인 기능을 제공하여 편리한 거래 가능! 경매를 통해 다양한 물건을 저렴하게 구매하거나 판매해보세요!",
+    };
+  },
+  components: {},
+};
+</script>
+
+<style scoped>
+.split-screen {
+  display: flex;
+  justify-content: space-between;
+}
+.v-line {
+  border-left: thin solid lightgrey;
+  height: 90%;
+  left: 50%;
+  margin-top: 20px;
+  position: absolute;
+}
+.left,
+.right {
+  flex: 1;
+  padding: 1em;
+  padding-right: 50px;
+  padding-left: 50px;
+}
+.product-img {
+  margin: 30%;
+}
+.card {
+  padding: 30px;
+  margin-bottom: 40px;
+}
+.bid-btn {
+  height: 60px;
+  margin-bottom: 30px;
+}
+.price-wrapper {
+  height: 120px;
+  display: flex;
+  align-items: center;
+}
+.max-price {
+  text-align: left;
+}
+.price {
+  font-size: x-large;
+  text-align: left;
+}
+.name {
+  text-align: left;
+  margin-bottom: 20px;
+}
+.date {
+  text-align: right;
+  margin-top: 15px;
+  margin-bottom: 15px;
+  font-weight: bold;
+  color: rgb(61, 61, 61);
+}
+.top {
+  top: 6%;
+  transform: translateY(-50%);
+  position: absolute;
+  margin-left: 250px;
+}
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+  z-index: 250;
+}
+.white-bg {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 50%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+.pwWrap {
+  width: 80%;
+  max-width: 450px;
+  background: lightgray;
+  margin: 20px auto;
+}
+.pwWrap .pwSection {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 150px;
+}
+.pwWrap .pwSection .dot {
+  display: block;
+  width: 10px;
+  height: 10px;
+  background: darkgrey;
+  border-radius: 100%;
+  margin: 0 5px;
+}
+.pwWrap .pwSection .dot.active {
+  background: rgba(0, 0, 0, 0.7);
+}
+.pwWrap .pwSection .message {
+  position: absolute;
+  bottom: 5px;
+  left: 0;
+  z-index: 1;
+  min-width: 100%;
+  text-align: center;
+  font-size: 14px;
+  font-weight: bold;
+  letter-spacing: -0.03em;
+  opacity: 0;
+  transition: 0.2s ease-out;
+}
+.pwWrap .numberSection {
+  overflow: hidden;
+}
+.pwWrap .numberSection .number {
+  float: left;
+  width: 33.33%;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  padding: 12px 0;
+  cursor: pointer;
+  background: whitesmoke;
+}
+.pwWrap .numberSection .number:last-child {
+  margin-left: 33.33%;
+}
+.pwWrap.error .message {
+  opacity: 1;
+  color: red;
+}
+.pwWrap.confirm .message {
+  opacity: 1;
+  color: green;
+}
+</style>
