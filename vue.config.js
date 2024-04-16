4bc611e3406674d9e922e6942b294cb34583ea21
DBuu_Kong <feel00k0810@gmail.com>
2024-04-16 13:20:14 +0900

    chore : 수정

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  oudputDir: "../wantit-be/src/main/resources/static",
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true
-      }
-    }
-  }
+  transpileDependencies: true
 })
