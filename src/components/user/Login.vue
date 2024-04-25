cc0c8cd152e2f75db7dd851dbd6bd2ed5b961d81
daramCoder <tjsdkyd4027@gmail.com>
2024-04-25 15:36:05 +0900

    :recycle: 사용되지 않는 기능 삭제

--- src/components/user/Login.vue
+++ src/components/user/Login.vue
@@ -17,9 +17,6 @@
         <div class="form-check text-start my-3">
           <input class="form-check-input" type="checkbox" value="remember-me"
                  id="flexCheckDefault">
-          <label class="form-check-label" for="flexCheckDefault">
-            자동 로그인
-          </label>
         </div>
         <div>
           <button type="submit" class="btn btn-primary w-100 py-2 sign-in-button" @click="fnLogin">
