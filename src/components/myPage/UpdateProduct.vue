490d399d9a226f67c7e8af10c410329e11f84bc6
dbfjru <dbfjru@gmail.com>
2024-04-26 15:50:29 +0900

    feat : image 업로드 추가 및 코드 정리

--- src/components/myPage/UpdateProduct.vue
+++ src/components/myPage/UpdateProduct.vue
@@ -9,7 +9,7 @@
         ❗ 판매 금지 품목은 경고 없이 삭제될 수 있습니다. ❗<br />
         ❗ 경매 시작일은 현재 날짜로부터 최소 1일 이후부터 설정 가능합니다.
         ❗<br />
-        ❗ 경매 종료는 시작당일 오후 7시에 자동으로 종료됩니다. ❗<br />
+        ❗ 경매 오픈 시각 09:00 , 경매 마감 시각 19:00 ❗<br />
       </p>
     </div>
   </div>
@@ -34,6 +34,30 @@
           class="form-control"
         ></textarea>
       </div>
+
+      <div class="mb-3">
+        <label for="productDescription" class="form-label">카테고리</label>
+        <div class="filter-bar">
+          <select
+            class="form-select"
+            id="country"
+            required=""
+            @change="changeCategory"
+          >
+            <option value="">선택</option>
+            <option value="SHOES">신발</option>
+            <option value="WATCH">시계</option>
+            <option value="LIMITED_EDITION">리미티드 에디션</option>
+            <option value="COLLABORATION">콜라보레이션</option>
+            <option value="FIGURE">피규어</option>
+            <option value="JEWELRY">쥬얼리</option>
+            <option value="ART">예술품</option>
+            <option value="ANTIQUE">엔티크</option>
+            <option value="ETC">기타</option>
+          </select>
+        </div>
+      </div>
+
       <div class="form-group">
         <label for="auctionStartPrice" class="form-label">경매 시작금액</label>
         <input
@@ -69,17 +93,19 @@
       <div class="form-group">
         <label for="productImage" class="form-label">상품 이미지</label>
         <input
+          @change="imageUpload()"
+          ref="images"
           type="file"
-          id="productImage"
-          @change="handleImageUpload"
           class="form-control"
+          id="productImage"
+          name="productImage"
         />
       </div>
 
       <button
         type="submit"
         class="btn btn-primary"
-        @click.prevent="updateProduct"
+        @click.prevent="updateProductWithImage"
       >
         수 정 하 기
       </button>
@@ -93,36 +119,47 @@ import axios from "axios";
 export default {
   data() {
     return {
+      today: this.getToday(),
       auctionItemId: undefined,
       itemName: "",
       itemDescription: "",
       minPrice: undefined,
       startDate: undefined,
       endDate: undefined,
+      category: "",
       productImage: "",
-      today: this.getToday(),
     };
   },
   methods: {
-    async updateProduct() {
+    imageUpload() {
+      this.imageFile = this.$refs.images.files[0];
+    },
+    async updateProductWithImage() {
       try {
+        const formData = new FormData();
+        const dto = {
+          itemName: this.itemName,
+          itemDescription: this.itemDescription,
+          minPrice: this.minPrice,
+          category: this.category,
+          startDate: this.startDate,
+          endDate: this.endDate,
+        };
+
+        const blob = new Blob([JSON.stringify(dto)], {
+          type: "application/json",
+        });
+
+        formData.append("file", this.imageFile);
+        formData.append("requestBody", blob);
+
         axios.put(
-          "https://api.dcns-wantit.shop/v1/my/auction-items/" + this.auctionItemId,
-          {
-            itemName: this.itemName,
-            itemDescription: this.itemDescription,
-            minPrice: this.minPrice,
-            startDate: this.convertToDate(this.startDate),
-            endDate: this.convertToDate(this.endDate),
-          },
+          "https://api.dcns-wantit.shop/v1/my/auction-items/" +
+            this.auctionItemId,
+          formData,
           {
             headers: {
               Authorization: localStorage.getItem("accessToken"),
-            },
-            proxy: {
-              protocol: "http",
-              host: "127.0.0.1",
-              port: 8080,
             },
           }
         );
@@ -132,27 +169,6 @@ export default {
         alert("경매 상품 정보 수정에 실패하엿습니다. 다시 시도해주세요.");
       }
     },
-    convertToDate(str) {
-      let parts = str.split("-");
-      let year = parseInt(parts[0], 10);
-      let month = parseInt(parts[1], 10) - 1;
-      let day = parseInt(parts[2], 10);
-
-      let date = new Date(year, month, day);
-      date.setHours(0, 0, 0, 0);
-
-      return date;
-    },
-    handleImageUpload(event) {
-      const file = event.target.file;
-      const reader = new FileReader();
-
-      reader.onload = (e) => {
-        this.productImage = e.target.result;
-      };
-
-      reader.readAsDataURL(file); // 파일 내용 읽기
-    },
     getToday() {
       let today = new Date();
       let dd = String(today.getDate() + 1).padStart(2, "0");
@@ -160,6 +176,49 @@ export default {
       let yyyy = today.getFullYear();
       today = yyyy + "-" + mm + "-" + dd;
       return today;
+    },
+    convertToDate(str, hour) {
+      let parts = str.split("-");
+      let year = parseInt(parts[0], 10);
+      let month = parseInt(parts[1], 10) - 1;
+      let day = parseInt(parts[2], 10);
+
+      let date = new Date(Date.UTC(year, month, day));
+      date.setUTCHours(hour, 0, 0, 0);
+
+      return date;
+    },
+    async changeCategory(event) {
+      const selectFilter = event.target.value;
+      switch (selectFilter) {
+        case "SHOES":
+          this.category = "SHOES";
+          break;
+        case "WATCH":
+          this.category = "WATCH";
+          break;
+        case "LIMITED_EDITION":
+          this.category = "LIMITED_EDITION";
+          break;
+        case "COLLABORATION":
+          this.category = "COLLABORATION";
+          break;
+        case "FIGURE":
+          this.category = "FIGURE";
+          break;
+        case "JEWELRY":
+          this.category = "JEWELRY";
+          break;
+        case "ART":
+          this.category = "ART";
+          break;
+        case "ANTIQUE":
+          this.category = "ANTIQUE";
+          break;
+        case "ETC":
+          this.category = "ETC";
+          break;
+      }
     },
   },
   created() {
