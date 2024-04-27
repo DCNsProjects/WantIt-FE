85737cc60ee425e4b9588897b56d698431955fcc
HEEEUN9812 <heeeun9812@gmail.com>
2024-04-28 00:12:31 +0900

    "낙찰내역 api 수정"

--- src/components/auction/MyFinishedAuction.vue
+++ src/components/auction/MyFinishedAuction.vue
@@ -53,7 +53,7 @@ export default {
 
     async getFinishedAuctionItems(page = 1) {
       axios
-      .get(`https://api.dcns-wantit.shop/v1/auction-items/finished?page=${page}&size=5`, {
+      .get(`https://api.dcns-wantit.shop/v1/my/auction-items/finished?page=${page}&size=5`, {
         proxy: {
           protocol: "http",
           host: "127.0.0.1",
